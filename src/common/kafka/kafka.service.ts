2172d7c9756e688c64f3ad9e22ab1a3971b9d7a1
PamelaRamirezA <sramirez.1506@gmail.com>
2023-10-17 12:17:55 -0500

    update

--- src/common/kafka/kafka.service.ts
+++ src/common/kafka/kafka.service.ts
@@ -7,6 +7,8 @@ import * as Kafka from 'node-rdkafka';
 @Injectable()
 export class KafkaService {
   private producer: Kafka.Producer = null;
+  private _isReady: boolean = false;
+  private _isConnected = false;
   constructor() {
     const kafkaBroker = process.env.KAFKA_BROKERS;
     const clientId = process.env.KAFKA_CLIENT_ID;
@@ -20,19 +22,44 @@ export class KafkaService {
       "sasl.mechanisms": "PLAIN",
       "sasl.username": user,
       "sasl.password": pwd,
-      "security.protocol": "sasl_ssl",
+      //"security.protocol": "sasl_ssl",
       "socket.keepalive.enable": true,
       "socket.blocking.max.ms": 100,
       "socket.nagle.disable": false,
     });
 
-    this.producer.connect();
-    this.producer.on('ready', () => {
-      console.log('Producer is ready');
-    });
+    this.producer.on('event.error', (err) => {
+      console.error('Error from producer');
+      console.error(err);
+    })
   }
 
+  async connect(): Promise<void> {
+    try {
+      this.producer.connect();
+      this._isConnected = true;
+      this.producer.on("ready", () => {
+        this._isReady = true;
+      });
+      this.producer.on("event.error", (err) => {
+        console.error("Error from producer:", err);
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  
   async produceMessage(topic: string, message: string) {
-    this.producer.produce(topic, -1, Buffer.from(message));
+    this.connect();
+    this.producer.on('ready', () => {
+      console.log('Producer is ready');
+      this._isReady = true;
+    });
+    if (!this._isReady) {
+      console.log("Producer is not ready");
+    }else{
+      this.producer.produce(topic, -1, Buffer.from(message));
+      this.producer.setPollInterval(100);
+    }
   }
 }
